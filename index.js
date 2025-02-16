4e7af014b985f3ac399db5a09442a16ebc983b50
roosterkombat <roosterkombat@gmail.com>
2025-02-16 22:59:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.listen(port, async () => {
 
 const bot = new Telegraf(BOT_TOKEN);
 
-const web_link = "https://rbat.netlify.app/";
+const web_link = "https://rbat.netlify.app";
 const community_link = "https://t.me/roosterkombat";
 
 
